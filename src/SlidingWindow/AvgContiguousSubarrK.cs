97c36ddce0e2b94297c09651918bcfb0f7a0d782
Tuong Vo <vomanhtuong@gmail.com>
2024-03-12 18:55:44 -0400

    Finish Average of contiguous subarray of size K

--- src/SlidingWindow/AvgContiguousSubarrK.cs
+++ src/SlidingWindow/AvgContiguousSubarrK.cs
@@ -1,7 +1,24 @@
 public class AvgContiguousSubarrK
 {
-    public static int[] Run(int[] arr)
+    public static double[] Run(int k, int[] arr)
     {
-        throw new NotImplementedException();
+        var result = new List<double>();
+        var windowSum = 0.0;
+        var windowStart = 0;
+
+        for (var windowEnd = 0; windowEnd < arr.Length; windowEnd++)
+        {
+            windowSum += arr[windowEnd];//add the new element
+
+            //slide the window until hit the size k
+            if (windowEnd >= k - 1)
+            {
+                result.Add(windowSum / k);//calculate the avg
+                windowSum -= arr[windowStart];//remove the element going out
+                windowStart += 1;//slide the window ahead, resizing window
+            }
+        }
+
+        return [.. result];
     }
 }
--- test/SlidingWindow.Tests/AvgContiguousSubarrKTest.cs
+++ test/SlidingWindow.Tests/AvgContiguousSubarrKTest.cs
@@ -5,7 +5,34 @@ public class AvgContiguousSubarrKTest
     public void Arr_Empty()
     {
         var arr = Array.Empty<int>();
-        var result = AvgContiguousSubarrK.Run(arr);
-        Assert.True(result.Length==0);
+        var k = 5;
+        var result = AvgContiguousSubarrK.Run(k, arr);
+        Assert.True(result.Length == 0);
+    }
+
+    [Fact]
+    public void Arr_5_K5()
+    {
+        int[] arr = [1, 3, 2, 6, -1];
+        var k = 5;
+        var result = AvgContiguousSubarrK.Run(k, arr);
+        Assert.Single(result);
+        Assert.Equal(2.2, result.First());
+
+    }
+
+    [Fact]
+    public void Arr_9_K5()
+    {
+        int[] arr = [1, 3, 2, 6, -1, 4, 1, 8, 2];
+        var k = 5;
+        var result = AvgContiguousSubarrK.Run(k, arr);
+        Assert.Collection(result,
+                        e => Assert.Equal(2.2, e, 1),
+                        e => Assert.Equal(2.8, e, 1),
+                        e => Assert.Equal(2.4, e, 1),
+                        e => Assert.Equal(3.6, e, 1),
+                        e => Assert.Equal(2.8, e, 1));
+
     }
 }
